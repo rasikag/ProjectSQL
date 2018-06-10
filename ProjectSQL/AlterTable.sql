1e7f6400061302353ae846ee71cb42147b7d4646
Rasika Gayan Gunarathna <rasikagayan90@gmail.com>
2018-06-10 22:42:06 +0800

    init commit

---
+++ ProjectSQL/AlterTable.sql
@@ -0,0 +1,4 @@
+﻿-- alter table 
+-- add new column 
+GO;
+ALTER TABLE TableName ADD NewColumnName FLOAT(10);
---
+++ ProjectSQL/CreateTable.sql
@@ -0,0 +1,26 @@
+﻿
+-- follow this naming convention
+-- template 
+CREATE TABLE TableName(
+	ID INT PRIMARY KEY IDENTITY(1,1),
+	Name VARCHAR(120) NOT NULL,
+	Quantity INT NOT NULL,
+	NumericFiledOne DECIMAL(6,2),
+	NumericFieldTwo FLOAT(10)
+);
+
+-- here identify the IDENTITY and increment value 
+-- type: INT 
+
+GO
+CREATE TABLE Groceries(
+	ID INT PRIMARY KEY IDENTITY(1,1),
+	Name VARCHAR(120) NOT NULL,
+	Quantity INT NOT NULL,
+	
+);
+
+GO 
+INSERT INTO [Groceries](Name,Quantity) VALUES ('Wholewheatbread',5);
+
+
