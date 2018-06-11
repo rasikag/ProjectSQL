5d73e888e6913796d2bdd9f5e1245175794ca47f
Rasika Gayan Gunarathna <rasikagayan90@gmail.com>
2018-06-11 22:14:21 +0800

    count

--- ProjectSQL/CHECKSUM_AGG_Temp.sql
+++ ProjectSQL/CHECKSUM_AGG_Temp.sql
@@ -1,7 +1,17 @@
 ﻿use AdventureWorks2012
 
+SELECT Quantity FROM Production.ProductInventory; 
+
 -- query template
 SELECT CHECKSUM_AGG(CAST(Quantity AS int))  
 FROM Production.ProductInventory;  
 GO  
+
+
+UPDATE Production.ProductInventory   
+SET Quantity=1000 
+WHERE Quantity=512;
+
+SELECT CHECKSUM_AGG(CAST(Quantity AS int))  
+FROM Production.ProductInventory; 
 
---
+++ ProjectSQL/Count.sql
@@ -0,0 +1,14 @@
+﻿use AdventureWorks2012
+
+-- template 
+-- COUNT(ColumnName) 
+-- this will return an int 
+
+-- template and example 
+SELECT COUNT(DISTINCT Title)  
+FROM HumanResources.Employee;  
+GO  
+
+SELECT COUNT(*)  
+FROM HumanResources.Employee;  
+GO 
